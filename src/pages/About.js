3002f6fb88672edf1134828c9c69f7718026c9d1
aliakbarnazemi22 <aliakbarnazemi2@gmail.com>
2025-05-24 08:42:54 +0330

    About , Skills Pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,56 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='md:px-0 px-4 container mx-auto mt-24'>About</div>
+    <div className='md:px-0 px-4 container mx-auto mt-24'>
+      <h2 className='text-3xl font-bold'>
+        Aliakbar Nazemi
+      </h2>
+      <p className='mt-1 font-[200]'>
+        Front end Developer
+      </p>
+
+      <p className='mt-6 text-lg font-[300] leading-[1.7]'>
+        <div className="sm:line-clamp-6 line-clamp-3">
+          About Me: Greetings! I'm Aliakbar Nazemi, a passionate 21-year-old front-end web developer hailing from the vibrant land of Iran. I embark on a journey through the ever-evolving digital landscape, weaving my dreams into the fabric of the World Wide Web.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          My Journey: My fascination with technology ignited at an early age, propelling me towards the captivating realm of web development. As a self-taught enthusiast, I've delved into the intricacies of front-end technologies, mastering the art of crafting visually appealing and seamless user experiences.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          Expertise: My expertise lies in the realm of front-end development, where I bring ideas to life through the harmonious fusion of HTML, CSS, and JavaScript. I revel in the challenge of transforming concepts into interactive and user-friendly interfaces that leave a lasting impression.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          Continuous Learning: In this dynamic field, I understand the importance of continuous learning. Whether it's staying abreast of the latest design trends or embracing cutting-edge frameworks, I am committed to honing my skills and adapting to the ever-shifting digital landscape.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          Vision: Beyond the lines of code, my vision extends to contributing positively to the tech community and leveraging my skills to create meaningful digital experiences. I aspire to play a role in shaping the future of web development, embracing innovation and pushing the boundaries of what's possible.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          Why Choose Me: Passion-Driven: My love for web development is the driving force behind every project I undertake. Attention to Detail: I believe in the power of details, ensuring that each pixel aligns with the overall vision and purpose of the project. Collaborative Spirit: I thrive in collaborative environments, valuing the synergy that emerges from diverse perspectives and ideas.
+        </div>
+        <div>
+          <br />
+        </div>
+        <div>
+          Let's Connect: Embark on this journey with me, and let's transform your digital vision into a captivating reality. Whether you're an individual with a unique idea or a business looking to establish a compelling online presence, I am here to turn concepts into captivating web experiences. Feel free to reach out, and let's create something extraordinary together!
+        </div>
+      </p>
+    </div>
   )
 }
 
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,9 +1,74 @@
 import React from 'react'
 
 function Skills() {
+    const cards = [
+        {
+            id: 1,
+            title: "HTML - CSS",
+            value: 100
+        },
+        {
+            id: 2,
+            title: "Bootstrap CSS",
+            value: 100
+        },
+        {
+            id: 3,
+            title: "Tailwind CSS",
+            value: 100
+        },
+        {
+            id: 4,
+            title: "Java Script",
+            value: 95
+        },
+        {
+            id: 5,
+            title: "React JS",
+            value: 95
+        },
+        {
+            id: 6,
+            title: "Next JS",
+            value: 95
+        },
+        {
+            id: 7,
+            title: "Swiper JS",
+            value: 100
+        },
+        {
+            id: 8,
+            title: "Type Script",
+            value: 70
+        },
+        {
+            id: 9,
+            title: "Git",
+            value: 60
+        }
+    ];
     return (
         <div>
-            <div className='md:px-0 px-4 container mx-auto mt-24'>Skills</div>
+            <div className='md:px-0 px-4 container mx-auto mt-24'>
+                <h2 className='ml-3 text-3xl font-bold text-start'>
+                    My Skills
+                </h2>
+
+                <div className='mt-1 flex items-center flex-wrap'>
+                    {cards.map((card) => (
+                        <div className='w-full md:w-1/2 lg:w-1/3 p-3'>
+                            <div id={card.id} className="w-full bg-zinc-700 shadow rounded-lg p-4 text-center min-h-[180px] flex items-center flex-col justify-center gap-4">
+                                <h3 className="text-2xl font-bold mt-2">{card.title}</h3>
+
+                                <div className="relative w-[90%] h-[15px] bg-zinc-800 rounded-full">
+                                    <div className="absolute h-full bg-white rounded-lg top-0 left-0" style={{ width: card.value + '%' }}></div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
